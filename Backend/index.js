15d6afc719fd029de6e0407e220e671673bc7e78
Om Prakash <ompra.rox@gmail.com>
2023-08-19 00:57:58 +0530

    PORt and MongoDB url added in dotenv

--- Backend/index.js
+++ Backend/index.js
@@ -1,20 +1,24 @@
 import express from "express";
 import { connect } from "./connection.js";
 import urlRoute from "./routes/url.js";
+import dotenv from "dotenv";
 
 const app = express();
-const PORT = 8001;
-const MONGO_URL =
-  "mongodb+srv://omprarox:ompra@cluster0.9mzjo1v.mongodb.net/?retryWrites=true&w=majority";
+// const PORT = 8003;
+
+if (process.env.NODE_ENV !== "production") {
+  //   require("dotenv").config();
+  dotenv.config();
+}
 
 // Connecting to MongoDB
-connect(MONGO_URL);
+connect(process.env.MONGO_URL);
 
 app.use(express.json());
 // all Routes
 app.use("/", urlRoute);
 
 // Starting the server
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on ${process.env.PORT}`);
 });
